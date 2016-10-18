4ba5e835496f0325e9f5bfd95508ad42b02ad04f
jhonatasfender <jhonata.fender@gmail.com>
2016-10-18 15:33:09 -0200

    add

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,6 @@
-
+var teste = 2;
 angular.module('starter', ['ionic', 'ngCordova'])
 .run(function($ionicPlatform) {
-  $ionicPlatform.linkHeard = "Search";
-  $ionicPlatform.link = "#/search";
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -26,21 +24,25 @@ angular.module('starter', ['ionic', 'ngCordova'])
     controller: 'SearchCtrl'
   });
   
-}).controller('MapCtrl',function($scope, $state, $cordovaGeolocation){
+})/*.controller('MapCtrl',function($scope, $state, $cordovaGeolocation){
   var options = {timeout: 10000, enableHighAccuracy: true};
-
+  console.log(teste);
+  teste = 5;
+  console.log(teste);
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
     
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         mapOptions = {
           center: latLng,
-          zoom: 15,
+          zoom: 16,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
       var marker = new google.maps.Marker({
           map: $scope.map,
+          draggable: true,
           animation: google.maps.Animation.DROP,
           position: latLng
       }), infoWindow = new google.maps.InfoWindow({
@@ -54,8 +56,38 @@ angular.module('starter', ['ionic', 'ngCordova'])
   }, function(error){
     console.log("Could not get location");
   });
-}).controller('SearchCtrl',function ($scope){
+})*/
+.controller('MapCtrl',function($scope, $ionicLoading) {
+  
   console.log("teste");
+  google.maps.event.addDomListener(window, 'load', function() {
+    console.log("teste");
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 20,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        var myLocation = new google.maps.Marker({
+            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            map: map,
+            title: "My Location"
+        });
+    });
+
+    $scope.map = map;
+  });
+})
+.controller('SearchCtrl',function ($scope){
+  console.log(teste);
+  teste = 1;
+  console.log(teste);
 }).controller('HomeCtrl',function ($scope){
 
   $scope.linkName = "Search";
