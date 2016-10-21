a90eaabbfa80b185a4c2c0ee1374e1b432988215
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-10-21 17:10:51 -0200

    implementing the animation
    implementing the animation to find the device location

--- www/js/app.js
+++ www/js/app.js
@@ -22,91 +22,54 @@ angular.module('starter', ['ionic', 'ngCordova'])
     url: '/search',
     templateUrl: 'templates/search.html',
     controller: 'SearchCtrl'
-  });
-  
+  });  
 })
-.controller('MapCtrl',function($scope, $state, $cordovaGeolocation){
-  var options = {timeout: 10000, enableHighAccuracy: true};
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+.controller('MapCtrl',function($scope, $state, $cordovaGeolocation) {
+  var options = {timeout: 10000, enableHighAccuracy: true}, divMap = document.getElementById("map"),
+  b = document.getElementById("buttonGEO"), w = window,d = document,e = d.documentElement,g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+  b.style.marginLeft = (x - 65) + "px";
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         mapOptions = {
+          mapTypeControl: false,
+          zoomControl: false,
+          scaleControl: false,
           center: latLng,
-          zoom: 16,
+          zoom: 10,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-    /*document.getElementById('mode').addEventListener('change', function() {
-      calculateAndDisplayRoute(directionsService, directionsDisplay);
-    });*/
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    /*for (var i = 0; i < 1; ++i) {
-      var others = new google.maps.Marker({
-        position: {
-          lat: -15.847113651199606,
-          lng: -47.98038952052593
-        },
-        map: $scope.map
-      });
-      attachSecretMessage(others, "teste");
-    }*/
-
-    function attachSecretMessage(marker, secretMessage) {
-      var infowindow = new google.maps.InfoWindow({
-        content: secretMessage
-      });
-      marker.addListener('click', function() {
-        infowindow.open(marker.get('map'), marker);
-      });
-    }
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-      directionsService.route({
-        origin: latLng,//{lat: 37.77, lng: -122.447},  // Haight.
-        destination: {lat: -15.847113651199606,lng: -47.98038952052593},  // Ocean Beach.
-        // Note that Javascript allows us to access the constant
-        // using square brackets and a string value as its
-        // "property."
-        /*       
-            DRIVING     => Driving
-            WALKING     => Walking
-            BICYCLING   => Bicycling
-            TRANSIT     => Transit
-
-         */
-        travelMode: google.maps.TravelMode["DRIVING"]
-      }, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
+    $scope.map = new google.maps.Map(divMap, mapOptions);
+    var directionsDisplay = new google.maps.DirectionsRenderer,directionsService = new google.maps.DirectionsService,
+    countZoom = 0,interval;
+    $scope.buttonGEO = function (){
+      countZoom = $scope.map.getZoom();
+      interval = window.setInterval(function() {
+        if(countZoom >= 10 && countZoom <= 16) { 
+          console.log(countZoom++);
+          $scope.map.setZoom(countZoom + 1);
+          mapOptions.zomm = $scope.map.getZoom();
         } else {
-          window.alert('Directions request failed due to ' + status);
+          clearTimeout(interval);
         }
-      });
-    }
-    directionsDisplay.setMap($scope.map);
-
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+      }, 2000);
+      $scope.map = new google.maps.Map(divMap, mapOptions); 
       var marker = new google.maps.Marker({
           map: $scope.map,
           draggable: true,
           animation: google.maps.Animation.DROP,
           position: latLng
-      }), infoWindow = new google.maps.InfoWindow({
-          content: "Here I am, go!"
       });
-      google.maps.event.addListener(marker, 'click', function () {
-        console.log("teste");
-        infoWindow.open($scope.map, marker);
-      });
-      google.maps.event.addDomListener($scope.map,'click',function (e){
-        console.log(e.latLng.lat());
-        console.log(e.latLng.lng());
-      });
+    };
+
+
+    google.maps.event.addDomListener($scope.map,'click',function (e){
+      console.log(e.latLng.lat());
+      console.log(e.latLng.lng());
     });
-  }, function(error){
-    console.log("Could not get location");
   });
+   
 })
 .controller('SearchCtrl',function ($scope){
   console.log(teste);
