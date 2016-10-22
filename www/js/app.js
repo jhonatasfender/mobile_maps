b76d1e1e6a8a2f6549f846d0fb60b581e6af43c9
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-10-22 13:11:14 -0200

    multiples markings

--- www/js/app.js
+++ www/js/app.js
@@ -39,31 +39,55 @@ angular.module('starter', ['ionic', 'ngCordova'])
           center: latLng,
           zoom: 10,
           mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+        },
+        beaches = [
+          ['Bondi Beach',    -15.831893364540038, -47.885284423828125],
+          ['Coogee Beach',   -15.785646183288632, -47.934722900390625],
+          ['Cronulla Beach', -15.80150355021274,  -47.91412353515625 ],
+          ['Manly Beach',    -15.812074438278657, -48.028106689453125],
+          ['Maroubra Beach', -15.814717074012279, -48.109130859375   ]
+        ];
     $scope.map = new google.maps.Map(divMap, mapOptions);
+    var markerOther = [], info = [];
+    for (var i = 0; i < beaches.length; i++) {
+      markerOther.push(new google.maps.Marker({
+          position: {lat: beaches[i][1], lng: beaches[i][2]},
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          title: beaches[i][0]
+      }));
+      info.push(new google.maps.InfoWindow({
+        content: beaches[i][0]
+      }));
+      markerOther[i].setMap($scope.map);
+      google.maps.event.addListener(markerOther[i], 'click', function(i) {
+        return function() {
+          info[i].open($scope.map, markerOther[i]);
+        }
+      }(i));
+    }
+    
     var directionsDisplay = new google.maps.DirectionsRenderer,directionsService = new google.maps.DirectionsService,
-    countZoom = 0,interval;
+    countZoom = 0,interval,marker = new google.maps.Marker();
     $scope.buttonGEO = function (){
+      marker.setMap(null);
       countZoom = $scope.map.getZoom();
       interval = window.setInterval(function() {
         if(countZoom >= 10 && countZoom <= 16) { 
           console.log(countZoom++);
           $scope.map.setZoom(countZoom + 1);
-          mapOptions.zomm = $scope.map.getZoom();
+          mapOptions.zomm = countZoom;
         } else {
           clearTimeout(interval);
         }
-      }, 2000);
-      $scope.map = new google.maps.Map(divMap, mapOptions); 
-      var marker = new google.maps.Marker({
-          map: $scope.map,
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: latLng
+      }, 500);
+      marker = new google.maps.Marker({
+        map: $scope.map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: latLng
       });
     };
-
-
     google.maps.event.addDomListener($scope.map,'click',function (e){
       console.log(e.latLng.lat());
       console.log(e.latLng.lng());
